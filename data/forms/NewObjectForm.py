50e4d993225bac8fc7aa53af83d7b1bbe9652ca5
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2022-04-21 16:07:27 +0700

    Added some validators

--- data/forms/NewObjectForm.py
+++ data/forms/NewObjectForm.py
@@ -1,18 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import FileField, StringField, SubmitField, TextAreaField, \
-    BooleanField
-from wtforms.validators import DataRequired, Length
+    BooleanField, IntegerField, SelectField
+from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class NewObjectForm(FlaskForm):
     from main import types, meanings
     name = StringField('Название',
                        validators=[DataRequired(), Length(max=300)])
-    region_id = StringField('Номер региона расположения',
-                            validators=[DataRequired()])
-    meaning_id = StringField(f'ID значения ({meanings})',
+    region_id = IntegerField('Номер региона расположения',
+                             validators=[DataRequired(),
+                                         NumberRange(min=1, max=94)])
+
+    meaning_id = SelectField(f'Значение ({meanings})', choices=meanings.keys(),
                              validators=[DataRequired()])
-    type_id = StringField(f'ID типа ({types})', validators=[DataRequired()])
+    type_id = SelectField(f'Тип ({types})', choices=types.keys(),
+                          validators=[DataRequired()])
     is_unesco = BooleanField('Находится в ЮНЕСКО')
     description = TextAreaField("Описание")
     video = FileField('Видео')
